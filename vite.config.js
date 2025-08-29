bc120b43d2b5b000a995eed0dc2d877935b4093f
swee-kar07 <sweekarayaj7@gmail.com>
2025-08-29 21:05:42 +0545

    Update base URL for exampage repository

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/physics-exam-portal/',  // Replace with your repository name
+  base: '/exampage/',  // Repository name
   build: {
     outDir: 'dist',
   },
